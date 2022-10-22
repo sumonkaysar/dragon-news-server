381fe1bd760c9ccb49a0b6839c8fc89d4973d642
Sumon Kaysar <msk.sumon.kaysar@gmail.com>
2022-10-22 17:34:43 +0600

    updated

--- index.js
+++ index.js
@@ -16,6 +16,26 @@ app.get('/news-categories', (req, res) => {
   res.send(categories)
 })
 
+app.get('/category/:id', (req, res) => {
+  const {id} = req.params
+  if (id === '08') {
+    res.send(news)
+  }else{
+    const categoryNews = news.filter(n => n.category_id === id)
+    res.send(categoryNews)
+  }
+})
+
+app.get('/news', (req, res) => {
+  res.send(news)
+})
+
+app.get('/news/:id', (req, res) => {
+  const {id} = req.params
+  const selectedNews = news.find(n => n._id === id)
+  res.send(selectedNews)
+})
+
 app.listen(port, () => {
   console.log('Dragon news server running on', port)
 })
